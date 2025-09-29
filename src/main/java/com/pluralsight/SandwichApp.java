e54070ec74839dce1470d47a330f9066af198c3b
Ahsan <sbaseer25@gmail.com>
2025-09-29 10:55:40 -0400

    first commit

---
+++ src/main/java/com/pluralsight/SandwichApp.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class SandwichApp {
+    public static void main(String[] args) {
+        System.out.println("This will be the sandwich app!");
+        }
+    }
