bbadd32b2de6d4a0bac9ec98dea148fe4a88bbcc
Ahsan <sbaseer25@gmail.com>
2025-09-29 11:35:02 -0400

    Ex 2 complete

--- src/main/java/com/pluralsight/SandwichApp.java
+++ src/main/java/com/pluralsight/SandwichApp.java
@@ -2,8 +2,6 @@ package com.pluralsight;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class SandwichApp {
     public static void main(String[] args) {
         //declared all my variables
